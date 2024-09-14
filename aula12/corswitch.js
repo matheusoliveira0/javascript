42a1642404c985705c9ccc41c41dcb31285a1e66
matheusoliveira0 <teumatheus.73@gmail.com>
2024-09-13 21:16:29 -0300

    a lot

---
+++ aula12/corswitch.js
@@ -0,0 +1,11 @@
+let color = "cinza";
+switch (color) {
+    case "vermelho":
+        console.log("Cor é vermelho");
+        break;
+    case "azul":
+        console.log("Cor é azul");
+        break;
+    default:
+        console.log("Cor desconhecida");
+}
