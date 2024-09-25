6b9ab19e1632ce09e83323bed725e0f6587fbaaa
matheusoliveira0 <teumatheus.73@gmail.com>
2024-09-25 15:19:18 -0300

    mts aulas

---
+++ aula13/ambienteteste.js
@@ -0,0 +1,13 @@
+// console.log('Td bem ')
+
+// var c = 100
+//     while (c <= 600) {
+//         console.log(`passo ${c}`)
+//         c++
+//     }
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <=1000)
